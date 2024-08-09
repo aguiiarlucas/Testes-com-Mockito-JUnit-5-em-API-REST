260d1727b061291f38e784e1eae8e7c2f581740d
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-08-09 14:55:43 -0300

    "Class User"

---
+++ api/src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.api.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
