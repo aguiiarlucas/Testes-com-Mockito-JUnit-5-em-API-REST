e79a0769ab489dfad5bf3d073428bf51d2fe64a6
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-08-09 15:43:28 -0300

    "UserRepository"

--- api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
         // Dummy implementation for demonstration purposes
-        return ResponseEntity.ok().body ( new User (1,"Bob","bob@gmail.com","123") );
+        return ResponseEntity.ok().body (userService.findById ( id ) );
     }
 }
---
+++ api/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> user = userRepository.findById ( id );
+        return user.orElse ( null );
+
+    }
+}
