e48ffc4d36759da3e574bd89eb5fbb20eae36c90
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-10-15 15:18:40 -0300

    "Create"

--- api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -7,11 +7,10 @@ import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,8 +29,18 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
-            List<User>list = service.findAll ();
-            List<UserDTO>userDTOList = list.stream().map (x->mapper.map ( x,UserDTO.class )).collect( Collectors.toList());
-            return ResponseEntity.ok ().body ( userDTOList );
+        var users = service.findAll ();
+        var userDTOs = users.stream ()
+                .map ( user -> mapper.map ( user, UserDTO.class ) )
+                .collect ( Collectors.toList () );
+        return ResponseEntity.ok ( userDTOs );
     }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest ()
+                .path ( "{/id}" ).buildAndExpand (service.create (obj).getId ()).toUri ();
+        return ResponseEntity.created ( uri ).build ();
+    }
+
 }
--- api/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ api/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.dicasdeumdev.api.services.impl;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
 import br.com.dicasdeumdev.api.services.UserService;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,8 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-
+    @Autowired
+    private ModelMapper mapper;
     @Autowired
     private UserRepository userRepository;
 
@@ -26,5 +29,10 @@ public class UserServiceImpl implements UserService {
     }
     public List<User> findAll(){
         return userRepository.findAll ();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return userRepository.save ( mapper.map ( obj,User.class ) );
     }
 }
