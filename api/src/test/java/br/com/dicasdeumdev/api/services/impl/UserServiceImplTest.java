efb7bc8f837125e0c0343cd8317ebca2ba800cdf
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-10-18 15:38:58 -0300

    "whenFindAllThenReturnAnListOfUsers"

--- api/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ api/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,6 +30,8 @@ class UserServiceImplTest {
     public static final String NAME = "Lucas";
     public static final String EMAIL = "lucascostaaguiar@hotmail.com";
     public static final String PASSWORD = "1234";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
     @InjectMocks
     private UserServiceImpl service;
     @Mock
@@ -61,18 +64,28 @@ class UserServiceImplTest {
 
     @Test
     void whenFindByIdThenReturnAnObjectNotFoundException() {//Quando for buscar pelo id,retorne um objeto não encontrado
-        when ( repository.findById ( anyInt () ) ).thenThrow ( new ObjectNotFoundException ( "Objeto não encontrado" ) );
+        when ( repository.findById ( anyInt () ) ).thenThrow ( new ObjectNotFoundException ( OBJETO_NAO_ENCONTRADO ) );
 
         try {
             service.findById ( ID );
         } catch (Exception e) {
             assertEquals ( ObjectNotFoundException.class, e.getClass () );
-            assertEquals ( "Objeto não encontrado", e.getMessage () );
+            assertEquals ( OBJETO_NAO_ENCONTRADO, e.getMessage () );
         }
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() { //Quando buscar todos, me retorne uma lista de usuarios {
+        when ( repository.findAll () ).thenReturn (List.of(user));
+        List<User>response = service.findAll ();
+        assertNotNull ( response );
+        assertEquals ( 1,response.size () );
+        assertEquals ( User.class,response.get(INDEX).getClass () );
+
+        assertEquals ( ID,response.get ( INDEX ).getId () );
+        assertEquals ( NAME,response.get (INDEX).getName () );
+        assertEquals ( EMAIL,response.get (INDEX).getEmail () );
+        assertEquals ( PASSWORD,response.get (INDEX).getPassword () );
     }
 
     @Test
