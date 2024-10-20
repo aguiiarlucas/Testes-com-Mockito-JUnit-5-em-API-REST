213cf57a568eea98dd85fc6628fbc3e3b3123a91
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-10-19 22:57:40 -0300

    "whenObjectNotFoundExceptionThenReturnAResponseEntity"

---
+++ api/src/test/java/br/com/dicasdeumdev/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,62 @@
+package br.com.dicasdeumdev.api.resources.exceptions;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
+import br.com.dicasdeumdev.api.resources.UserResource;
+import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
+import br.com.dicasdeumdev.api.services.impl.UserServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+
+    public static final String RESOURCE_NOT_FOUND = "Resource not found ";
+    @InjectMocks
+    private ResourceExceptionHandler resourceExceptionHandler;
+    private User user;
+    private UserDTO userDTO;
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks ( this );
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = resourceExceptionHandler.objectNotFound
+                ( new ObjectNotFoundException ( OBJETO_NAO_ENCONTRADO ), new MockHttpServletRequest () );
+        StandardError error = response.getBody ();
+
+        assertNotNull ( response );
+        assertNotNull ( response.getBody () );
+        assertNotNull ( OBJETO_NAO_ENCONTRADO,response.getBody ().getError () );
+
+        assertEquals ( HttpStatus.NOT_FOUND,response.getStatusCode () );
+        assertEquals ( ResponseEntity.class,response.getClass () );
+        assertEquals ( StandardError.class,response.getBody ().getClass () );
+        assertEquals ( 404,response.getBody ().getStatus () );
+        assertEquals ( RESOURCE_NOT_FOUND,error.getError () );
+    }
+
+    @Test
+    void dataIntegratyViolationException() {
+    }
+
+
+}
